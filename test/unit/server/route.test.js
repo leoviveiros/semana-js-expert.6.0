7e07a457b6877509dee5d4398af2b6dd2e087d48
Leonardo Pinto <leoviveiros@gmail.com>
2022-03-18 16:45:35 -0300

    todos route tests

--- test/unit/server/route.test.js
+++ test/unit/server/route.test.js
@@ -1,7 +1,22 @@
-import { describe, test, expect } from '@jest/globals';
+import { describe, test, expect, jest, beforeEach } from '@jest/globals';
+import config from '../../config';
 
-describe('#Routes', () => {
-    test('my test', () => { 
-        expect(1).toBe(1);
+const { pages } = config;
+
+describe('#Routes - test suite for api response', () => {
+    beforeEach(() => {
+        jest.restoreAllMocks();
+        jest.clearAllMocks();
+    });
+
+    test.todo('GET / - should redirect do home page');
+    test.todo('GET /home - should return home/index.html stream');
+    test.todo('GET /controller - should return controller/index.html stream');
+    test.todo('GET /<any-existing-file> - should return a file stream');
+    test.todo('GET /unknown - should return a 404 response');
+
+    describe('exceptions', () => { 
+        test.todo('given inexistent file it should respond with 404');
+        test.todo('given an error it should respond with 500');
     })
 })
