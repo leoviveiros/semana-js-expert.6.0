465241b574727b71be060d2c7eba2f50dbb1c802
Leonardo Pinto <leoviveiros@gmail.com>
2022-03-19 13:06:43 -0300

    inclusao testes service

---
+++ test/unit/server/service.test.js
@@ -0,0 +1,52 @@
+import { describe, test, expect, jest, beforeEach } from '@jest/globals';
+
+import { Service } from '../../../server/service';
+
+import config from '../../../server/config';
+import TestUtils from '../test-utils';
+
+describe('#Service - test suite for business rules and processing', () => {
+
+    beforeEach(() => {
+        jest.restoreAllMocks();
+        jest.clearAllMocks();
+    });
+
+    test('createFileStream shoud return a readable stream', async () => {
+        const mockFileStream = TestUtils.generateReadableStream(['data']);
+
+        const service = new Service();
+
+        jest.spyOn(service, 'createFileStream')
+            .mockReturnValue(mockFileStream);
+
+        const expectedFileStream = service.createFileStream('index.html');
+
+        expect(expectedFileStream).toBe(mockFileStream);
+    });
+
+    test('getFileInfo should return a file info object from home/index.html', async () => {
+        const fileName = config.pages.homeHtml;
+
+        const service = new Service();
+
+        const expectedFileInfo = await service.getFileInfo(fileName);
+
+        expect(expectedFileInfo.name).toMatch(new RegExp(`public\\\\home\\\\index.html$$`)); // ends with home/index.html
+        expect(expectedFileInfo.type).toBe('.html');
+    });
+
+    test('getFileStream should return a file stream from home/index.html', async () => {
+        const mockFileStream = TestUtils.generateReadableStream(['data']);
+
+        const service = new Service();
+
+        jest.spyOn(service, 'createFileStream').mockReturnValue(mockFileStream);
+
+        const result = await service.getFileStream('home/index.html');
+
+        expect(result.stream).toBe(mockFileStream);
+        expect(result.type).toBe('.html');
+    });
+
+})
